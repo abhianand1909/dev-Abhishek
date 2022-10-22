c0f4456996fd85a6790672ed7f03199f408fdf70
Abhishek <abhianand1909@gmail.com>
2022-10-22 15:44:56 +0530

    Exercise 1

--- MyApp/src/App.js
+++ MyApp/src/App.js
@@ -4,72 +4,26 @@ import Comp from "./Component/Comp";
 import Displayname from "./Component/Displayname";
 
 function App() {
-  // const arr=["Rajnish","Raj","Anand","Aditya"];
-  // let [condition,changecondition]=useState(true);
-  // let [count,incCount]=useState(0);
-  // function increment()
-  //   {
-  //     if(condition==true){
-  //     incCount(count=count+2);
-  //         if(count==10)
-  //         {
-  //           changecondition(condition=false);
-  //         }
-  //     }
-
-  //     else if(condition==false){ 
-  //     incCount(count=count-2);
-  //         if (count==0)
-  //         {
-  //           changecondition(condition=true);
-  //         }
-  //      }
-      
-      
-  //   }
-  let[arr,setArr]=useState([]);
-  let [value,getValue]=useState('');
-  let Display =(elem) =>
-  {
-    getValue(elem.target.value);
+  let [value,getvalue]= useState();
+  let [display,setvalue]=useState();
+  let inputvalue =(elem) =>{
+    getvalue(elem.target.value);
   }
-  let submit=() =>
-  {
-    setArr([value,...arr]);
-    console.log(arr);
+  let extractvalue=() =>{
+        setvalue(display=value);
   }
-
+ 
   return (
     <>
-    <div> hii <Displayname/></div>
-    <div className="hello">
-
-      {/* <Comp name="Abhishek"/> <Comp name="Anand"/> <Comp name="Aditya"/> */}
-      {/* {Comp("Abhi")}{Comp("Ravi")}{Comp("Raghav")} */}
-      {/* {
-        
-       arr.map((elem)  => 
-        {
-          return (
-             <Comp name={elem}/>
-          )
-        })
-      } */}
-      {/* <div>{count}</div>
-    <button onClick={increment}>Click</button> */}
-    <div>{value}</div>
-    <input type="text" value={value} onChange={Display} />
-    <button onClick={submit}>Submit</button>
-   { arr.map((element)=>
-    {
-      return(
-        <div>{element}</div>
-      )
-    })}
-    </div>
-
     
+    <div className="Exercise1">
+       <h1> EXERCISE 1</h1>     
+        <input type="text" value={value} onChange={inputvalue} />
+      <div>{display}</div>
+      <button onClick={extractvalue}>Submit</button>
+    </div>
     </>
   );
 }
 export default App;
+
