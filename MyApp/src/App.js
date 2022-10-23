be32e0845360ac54f8866720111ffab2ef52dd2f
Abhishek <abhianand1909@gmail.com>
2022-10-23 11:42:24 +0530

    Exercise 2

--- MyApp/src/App.js
+++ MyApp/src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
-import Comp from "./Component/Comp";
-import Displayname from "./Component/Displayname";
+import Table from "./Component/Table"
 
-function App() {
+
+function App(){
+
+  // Exercise 1
   let [value,getvalue]= useState();
   let [display,setvalue]=useState();
   let inputvalue =(elem) =>{
@@ -12,18 +14,30 @@ function App() {
   let extractvalue=() =>{
         setvalue(display=value);
   }
+
+  // Exercise 2
+
+   let students=[{Name:"Ram" ,Age:"27"},{Name:"Rajnish" ,Age:"26"},{Name:"Raj" ,Age:"25"},{Name:"Anand" ,Age:"28"}];
+   let columns=["Name","Age"];
+
+  // Exercise 3
+  
  
   return (
     <>
     
     <div className="Exercise1">
-       <h1> EXERCISE 1</h1>     
+       <h3> EXERCISE 1</h3>     
         <input type="text" value={value} onChange={inputvalue} />
+        <button onClick={extractvalue}>Submit</button>
       <div>{display}</div>
-      <button onClick={extractvalue}>Submit</button>
+    </div>
+
+    <div className="Exercise2">
+      <h3 className="E2">EXERCISE 2</h3>
+    <Table students={students} colNames={columns}/>
     </div>
     </>
   );
 }
 export default App;
-
