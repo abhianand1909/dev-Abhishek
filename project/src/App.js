582cfb2a095cb11197af8fce9b44b9a3c7a5a38a
Abhishek <abhianand1909@gmail.com>
2022-11-16 09:11:29 +0530

    Cricket Project

--- project/src/App.js
+++ project/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import PlayersData from './PlayersData.json'
 
 function App() {
+  let [playerData,getPlayerData] =useState(PlayersData[0]);
+
+  let sendData =((pindex) => 
+  {
+    getPlayerData(PlayersData[pindex])
+    
+  })
+  
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='ictHeading'>Indian Cricket Team</div>
+    <div>
+        <div className='playerDetails'>
+         <img src={playerData.image} className="playerImage" alt={playerData.Name} />
+         <div className='PlayerDetails'>
+          <div> Name -{playerData.Name}</div>
+          <div>Age -{playerData.Age}</div> 
+          <div>Test Runs -{playerData.TestRuns}</div>
+          <div>ODI Runs -{playerData.ODIRuns}</div>
+          <div>T20 Runs -{playerData.T20Runs}</div>
+          <div>Profile</div>
+          <div>{playerData.Profile}</div>
+        </div> 
     </div>
+      
+      <div className='pIconData'>
+      {
+      PlayersData.map((data ,indexNumber) => {
+        return(
+          <div className='pIcon' onClick={() =>{sendData(indexNumber)}}>
+            <img className='pImage' src={data.image} alt="" />
+            <div className='pName'>{data.Name}</div>
+          </div>
+        )
+      })
+    } 
+      </div>
+    </div>
+    </>
   );
 }
 
