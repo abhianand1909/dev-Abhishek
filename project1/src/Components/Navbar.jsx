12c00488760292ead62841a21af253b7ab757631
Abhishek <abhianand1909@gmail.com>
2022-11-17 17:06:16 +0530

    SearchBar

--- project1/src/Components/Navbar.jsx
+++ project1/src/Components/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import HomePageDress from '../json/HomePageDress.json'
+
 
 const Navbar = () => {
   return (
     <>
     <div className='navbar'>
         <div className='logoDiv'><img className='logo' src="./Images/Navbar/saga logo 2_1.jpg" alt="" /></div>
-        <div className='searchBar'>
-          <input className='searchBarText' type="text" />
-          <img className='searchBarIcon' src="./Images/Navbar/SearchIcon.png" alt="" />
+        
+        <div className='searchBox'>
+          <input className='searchBarText' type="text" placeholder='Search'/>
+          <i className='fas fa-search' ></i>
+          <div className='container'></div>
         </div>
     </div> 
     </>
